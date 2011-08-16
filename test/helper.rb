719ce297e9c2958e15aad4deaf18cd8797c3932b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2011-08-17 01:00:20 +0900

    Add some tests for Extractor

--- test/helper.rb
+++ test/helper.rb
@@ -1,2 +1,23 @@
 gem 'test-unit'
 require 'test/unit'
+require 'css_inliner'
+
+class CSSInlinerTestCase < Test::Unit::TestCase
+  include CSSInliner
+
+  def setup
+    @fixtures_dir = File.join(File.dirname(__FILE__), 'fixtures')
+
+    @sample1_dir = File.join(@fixtures_dir, 'sample1')
+    @sample2_dir = File.join(@fixtures_dir, 'sample2')
+    @sample3_dir = File.join(@fixtures_dir, 'sample3')
+
+    @html1 = File.read(File.join(@sample1_dir, 'index.html'))
+    @html2 = File.read(File.join(@sample2_dir, 'index.html'))
+    @html3 = File.read(File.join(@sample3_dir, 'index.html'))
+
+    @doc1 = Nokogiri.HTML @html1
+    @doc2 = Nokogiri.HTML @html2
+    @doc3 = Nokogiri.HTML @html3
+  end
+end
--- test/test_extractor.rb
+++ test/test_extractor.rb
@@ -1,19 +1,53 @@
 require_relative 'helper'
 require 'nokogiri'
-require 'css_inliner/extractor'
 
-class ExtractorTest < Test::Unit::TestCase
-  include CSSInliner
-
+class ExtractorTest < CSSInlinerTestCase
   def setup
-    @sample_html = File.join File.dirname(__FILE__), 'fixtures', 'sample1', 'index.html'
-    doc = Nokogiri.HTML File.read(@sample_html)
-    @extractor = Extractor.new doc, File.dirname(@sample_html)
+    super
+    @extractor1 = Extractor.new @doc1, @sample1_dir
+    @extractor2 = Extractor.new @doc2, @sample2_dir
+    @extractor3 = Extractor.new @doc3, @sample3_dir
   end
 
   def test_extract_from_link_basic
-    expected = File.read(File.join(File.dirname(@sample_html), 'rdoc.css'))
-    actual = @extractor.extract_from_link[0]
+    expected = File.read(File.join(@sample1_dir, 'rdoc.css'))
+    actual = @extractor1.extract_from_link[0]
     assert_equal expected, actual
+  end
+
+  def test_extract_from_link_remove_link_element_when_passed_true
+    @extractor1.extract_from_link(true)
+    assert_empty @doc1.css('link[rel="stylesheet"]')
+  end
+
+  def test_extract_from_link_leave_link_element_when_passed_false
+    expected = @doc1.css('link[rel="stylesheet"]').to_s
+    @extractor1.extract_from_link(false)
+    assert_equal expected, @doc1.css('link[rel="stylesheet"]').to_s
+  end
+
+  def test_extract_from_style_basic
+    src = '
+    h2   {
+      color:  gray ;
+    }
+'
+    expected = CssParser::Parser.new
+    expected.add_block! src
+    actual = CssParser::Parser.new
+    actual.add_block! @extractor3.extract_from_style[0]
+
+    assert_equal expected.to_s, actual.to_s
+  end
+
+  def test_extract_from_style_remove_style_element_when_passed_true
+    @extractor3.extract_from_style(true)
+    assert_empty @doc3.css('style')
+  end
+
+  def test_extract_from_style_leave_style_element_when_passed_false
+    expected = @doc3.css('style').to_s
+    @extractor3.extract_from_style(false)
+    assert_equal expected, @doc3.css('style').to_s
   end
 end
