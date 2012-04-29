be0c22b6a6404adbb8eb7ad218572a51fbb11447
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-04-29 09:14:59 +0900

    Use methods regarding merge

--- lib/css_inliner/inliner.rb
+++ lib/css_inliner/inliner.rb
@@ -14,18 +14,39 @@ module CSSInliner
     end
 
     def inline
+      original_styles = {}
+      @document.search('.//*[@style]').each do |elem|
+        original_styles[elem] = elem['style']
+        elem.remove_attribute('style')
+      end
+
       css = @extractor.extract
-      css.ordered_selectors.reverse_each do |selector|
+      css.ordered_selectors.each do |selector|
         sel = selector.to_s
         next if sel =~ /@|:/
-        body = @document.css('body')
-        body.css(sel).each do |elem|
-          elem = body.css('body').first if elem.name == 'html'
-          next unless elem
-          # To to: implement Declaration#merge and calc performance
-          elem['style'] = "#{selector.declarations.join}#{elem['style']}"
+        sel = 'body' if sel == '*' or sel == 'html'
+        @document.css(sel).each do |elem|
+          base = CSSPool.CSS("* {#{elem['style']}}").rule_sets.first.declarations
+          elem['style'] = CSSPool::CSS.update_declarations(base, selector.declarations).join
         end
       end
+
+      original_styles.each_pair do |elem, style|
+        base = CSSPool.CSS("* {#{elem['style']}}").rule_sets.first.declarations
+        orig = CSSPool.CSS("* {#{style}}").rule_sets.first.declarations
+        elem['style'] = CSSPool::CSS.update_declarations(base, orig).join
+      end
+      # css.ordered_selectors.reverse_each do |selector|
+      #   sel = selector.to_s
+      #   next if sel =~ /@|:/
+      #   body = @document.css('body')
+      #   body.css(sel).each do |elem|
+      #     elem = body.css('body').first if elem.name == 'html'
+      #     next unless elem
+      #     # To to: implement Declaration#merge and calc performance
+      #     elem['style'] = "#{selector.declarations.join}#{elem['style']}"
+      #   end
+      # end
 
       @document
     end
