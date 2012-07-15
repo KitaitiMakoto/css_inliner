2540bb9600f326a75603395c541e09247906c7d9
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-07-15 20:54:23 +0900

    Implement Declaration#expand_border partially: border width

--- test/test_csspool.rb
+++ test/test_csspool.rb
@@ -36,7 +36,55 @@ EOC
     assert_equal 'color: blue !important;', base.to_css.strip
   end
 
-  def test_non_dimension_expandable
+  def test_non_expandable_border
+    non_expandable = CSSPool.CSS <<EOC
+p {
+  border-top-width: 12em;
+}
+EOC
+    expected = CSSPool.CSS <<EOC
+p {
+  border-top-width: 12em;
+}
+EOC
+    assert_equal expected.rule_sets.first.declarations.to_s,
+                 non_expandable.rule_sets.first.declarations.first.expand_border.to_s
+  end
+
+  def test_expand_five_dimension_border_width
+    five_border_dimensions = CSSPool.CSS <<EOC
+p {
+  border-width: 12em 3em 1em 5em 3em;
+}
+EOC
+    assert_raise CSSPool::CSS::Declaration::InvalidExpressionCountError do
+      five_border_dimensions.rule_sets.first.declarations.first.expand_border
+    end
+  end
+
+  def test_expand_four_dimension_border_width
+    four_border_dimensions = CSSPool.CSS <<EOC
+p {
+  border-width: 1em 2em 3em 4em
+}
+EOC
+    expected = CSSPool.CSS <<EOC
+p {
+  border-top-width: 1em;
+  border-right-width: 2em;
+  border-bottom-width: 3em;
+  border-left-width: 4em;
+}
+EOC
+    assert_equal expected.rule_sets.first.declarations.to_s,
+                 four_border_dimensions.rule_sets.first.declarations.first.expand_border.to_s
+  end
+
+  def test_expand_three_dimension_border_width
+    assert false
+  end
+
+  def test_non_expandable_dimension
     no_dimension = CSSPool.CSS <<EOC
 p {
   color: blue;
