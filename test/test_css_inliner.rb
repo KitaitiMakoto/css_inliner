2aeb0ba259173b56b2c5cde3128834d1a04a0b3e
KITAITI Makoto <KitaitiMakoto@gmail.com>
2012-07-17 03:23:14 +0900

    Correct old tests

--- test/test_css_inliner.rb
+++ test/test_css_inliner.rb
@@ -1,4 +1,5 @@
 require_relative 'helper'
+require 'tmpdir'
 require 'css_inliner'
 
 class CSSInlinerTest < CSSInlinerTestCase
@@ -7,32 +8,111 @@ class CSSInlinerTest < CSSInlinerTestCase
   end
 
   def test_process_without_inline_style
-    pend
-
-    assert_equal *process(@sample1_dir)
+    html = <<EOH
+<html>
+  <head>
+    <title>Without Inline Style</title>
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+  </head>
+  <body>
+    <p>This is a blue line.</p>
+  </body>
+</html>
+EOH
+    css = <<EOC
+p {
+  color: blue;
+}
+EOC
+    inlined = process(html, css)
+    doc = Nokogiri.HTML(inlined)
+    style = doc.search('p').first['style'].strip
+    assert_equal 'color: blue;', style
   end
 
   def test_process_with_inline_style
-    pend
-
-    assert_equal *process(@sample2_dir)
+    html = <<EOH
+<html>
+  <head>
+    <title>With Inline Style</title>
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+  </head>
+  <body>
+    <p style="font-weight: bold;">This is a bold blue line.</p>
+  </body>
+</html>
+EOH
+    css = <<EOC
+p {
+  color: blue;
+}
+EOC
+    inlined = process(html, css)
+    doc = Nokogiri.HTML(inlined)
+    style = doc.search('p').first['style'].strip.gsub(/ +/, '')
+    assert_equal 'color:blue;font-weight:bold;', style
   end
 
-  def test_process_with_style_attribute
-    pend
-
-    assert_equal *process(@sample3_dir)
+  def test_process_with_style_elem
+    html = <<EOH
+<html>
+  <head>
+    <title>With Style Attribute</title>
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <style type="text/css">
+      p {font-style: oblique;}
+    </style>
+  </head>
+  <body>
+    <p>This is a oblique blue line.</p>
+  </body>
+</html>
+EOH
+    css = <<EOC
+p {
+  color: blue;
+}
+EOC
+    inlined = process(html, css)
+    doc = Nokogiri.HTML(inlined)
+    style = doc.search('p').first['style'].strip.gsub(/ +/, '')
+    assert_equal 'color:blue;font-style:oblique;', style
   end
 
-  def test_process_with_inline_style_and_style_attibute
-    pend
-
-    assert_equal *process(@sample4_dir)
+  def test_process_with_inline_style_and_style_elem
+    html = <<EOH
+<html>
+  <head>
+    <title>With Inline Style and Style Attribute</title>
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <style type="text/css">
+      p {font-style: oblique;}
+    </style>
+  </head>
+  <body>
+    <p style="font-weight: bold;">This is a bold oblique blue line.</p>
+  </body>
+</html>
+EOH
+    css = <<EOC
+p {
+  color: blue;
+}
+EOC
+    inlined = process(html, css)
+    doc = Nokogiri.HTML(inlined)
+    style = doc.search('p').first['style'].strip.gsub(/ +/, '')
+    assert_equal 'color:blue;font-style:oblique;font-weight:bold;', style
   end
 
-  def process(basedir)
-    source = File.read(File.join(basedir, 'index.html'))
-    inlined = File.read(File.join(basedir, 'index.inlined.html'))
-    [inlined, CSSInliner.process(source, basedir)]
+  private
+
+  def process(html, css)
+    Dir.mktmpdir('css_inliner') do |dir|
+      File.open("#{dir}/style.css", 'w') do |f|
+        f.write css
+      end
+      CSSInliner.process(html, dir)
+    end
   end
 end
